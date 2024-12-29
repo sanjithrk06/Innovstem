ff5e809e2a0258ce03658d843aeabd6bf41c4330
sanjithrk06 <sanjithkarthikeyanr@gmail.com>
2024-12-29 23:16:45 +0530

    Main Layout Setup completed

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,98 @@
 import React from "react";
+import { Mail } from "lucide-react";
+import { FaLinkedinIn } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className=" shadow-2xl p-10">
+      <div className="flex flex-row">
+        <div className="w-5/12 flex flex-col items-start gap-3">
+          <span className="logo">Innovstem</span>
+          <p className=" nav-content">
+            Innovstem is a dynamic startup transforming everyday products by
+            offering innovative, eco-friendly alternatives to traditional
+            plastics.{" "}
+          </p>
+          <span className="">
+            © Copyright {new Date().getFullYear()}{" "}
+            <a href="/#" className="hover:underline">
+              Innovstem
+            </a>
+          </span>
+        </div>
+        <div className="w-4/12 border-x-2 px-4 pb-2 flex flex-col items-center justify-start gap-4">
+          <h2 className=" nav-title">Navigation</h2>
+          <div className="flex flex-col items-center justify-center gap-2">
+            <div className=" flex flex-row justify-center items-start text-start gap-6">
+              <ul className=" flex flex-col flex-wrap gap-2 pl-4 font-normal">
+                {[
+                  "About Us",
+                  "Serices",
+                  "Courses & Webinars",
+                  "Resources & Blogs",
+                ].map((item, index) => (
+                  <li key={item}>
+                    <a
+                      href={`/${item.toLowerCase().replace(" ", "-")}`}
+                      className=" nav-content hover:text-primary"
+                    >
+                      {item}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+              <ul className=" flex flex-col flex-wrap gap-2 pl-4 font-normal">
+                {["Industrial Connection"].map((item, index) => (
+                  <li key={item}>
+                    <a
+                      href={`/${item.toLowerCase().replace(" ", "-")}`}
+                      className="nav-content hover:text-primary"
+                    >
+                      {item}
+                    </a>
+                  </li>
+                ))}
+                <button className="bg-primary/10 my-3 text-primary text-base font-noto font-medium cursor-pointer py-2 px-5 w-auto rounded-full duration-300 z-10 flex flex-row group ease-in">
+                  Go to Dashboard
+                </button>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="w-3/12 text-center flex flex-col gap-2">
+          <h2 className=" nav-title">Social Media</h2>
+          <p className=" nav-content">+123 4567 9876</p>
+          <p className=" nav-content">innovstem@gmail.com</p>
+          <ul className="flex justify-center gap-1 space-x-2">
+            {[
+              {
+                icon: (
+                  <FaLinkedinIn className="w-7 h-7 text-white bg-primary rounded-xl p-1" />
+                ),
+                href: "https://www.linkedin.com/in/ranjith-kumar-156706171/",
+              },
+              {
+                icon: (
+                  <Mail className="w-7 h-7 text-white bg-primary rounded-xl p-1" />
+                ),
+                href: "mailto:ranjith@ecologicads.in",
+              },
+            ].map((social, index) => (
+              <li key={index}>
+                <a
+                  href={social.href}
+                  target="_blank"
+                  className="text-[#39569c] hover:text-gray-900"
+                >
+                  {social.icon}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
 
   return (
     <nav className="border-gray-200 bg-gradient-to-b from-secondary via-secondary/70 to-secondary/20 backdrop-blur-3xl drop-shadow-2xl shadow-secondary/10 sticky top-0 z-40 transition-all duration-300">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div className="max-w-screen-2xl px-16 flex flex-wrap items-center justify-between mx-auto py-6">
         <a
           href="/"
           className="flex items-center space-x-3 rtl:space-x-reverse group"
@@ -24,8 +24,8 @@ const Header = () => {
             className="w-[45px] transform transition-transform duration-300"
             alt="Eco-logicAds Logo"
           /> */}
-          <span className="logo-attribute transform transition-all duration-300">
-            Eco-logicAds
+          <span className="logo transform transition-all duration-300">
+            Innovstem
           </span>
         </a>
 
@@ -65,7 +65,7 @@ const Header = () => {
           } w-full md:block md:w-auto overflow-hidden transition-all duration-500 ease-in-out`}
           id="navbar-solid-bg"
         >
-          <ul className="flex flex-col font-medium mt-4 rounded-lg bg-transparent text-center md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent gap-2">
+          <ul className="flex flex-col font-medium mt-4 rounded-lg bg-transparent text-center md:space-x-8 rtl:space-x-reverse md:flex-row md:-mt-1 md:border-0 md:bg-transparent gap-2">
             <li
               className="transform transition-all duration-300 hover:translate-x-1 md:hover:translate-x-0 pt-2"
               onClick={closeMenu}
@@ -77,7 +77,7 @@ const Header = () => {
                     group ${
                       isActive
                         ? "text-primary bg-green-50/60 md:bg-transparent md:text-primary"
-                        : "text-text/90 hover:bg-primary md:hover:bg-transparent md:hover:text-primary"
+                        : "text-text/90 hover:bg-primary md:hover:bg-transparent md:hover:text-primary nav-content"
                     }`
                 }
               >
@@ -99,11 +99,11 @@ const Header = () => {
                     group ${
                       isActive
                         ? "text-primary bg-green-50/60 md:bg-transparent md:text-primary"
-                        : "text-text/90 hover:bg-primary md:hover:bg-transparent md:hover:text-primary"
+                        : "text-text/90 hover:bg-primary md:hover:bg-transparent md:hover:text-primary nav-content"
                     }`
                 }
               >
-                Our Products
+                Services
                 <span
                   className={`absolute bottom-0 left-0 w-0 h-0.5 bg-primary transition-all duration-300 ease-out 
                                  group-hover:w-full md:opacity-0 md:group-hover:opacity-100 `}
@@ -111,15 +111,76 @@ const Header = () => {
               </NavLink>
             </li>
             <li
-              className="transform transition-all duration-300 hover:translate-x-1 md:hover:translate-x-0"
+              className="transform transition-all duration-300 hover:translate-x-1 md:hover:translate-x-0 pt-2"
               onClick={closeMenu}
             >
-              <NavLink to={"/contact/"}>
-                <button className="btn-primary m-1">Inquiry</button>
+              <NavLink
+                to={"/our-products/"}
+                className={({ isActive }) =>
+                  `block py-2 px-3 md:p-0 rounded relative transform transition-all duration-300 ease-in-out 
+                    group ${
+                      isActive
+                        ? "text-primary bg-green-50/60 md:bg-transparent md:text-primary"
+                        : "text-text/90 hover:bg-primary md:hover:bg-transparent md:hover:text-primary nav-content"
+                    }`
+                }
+              >
+                Courses & Webinars
+                <span
+                  className={`absolute bottom-0 left-0 w-0 h-0.5 bg-primary transition-all duration-300 ease-out 
+                                 group-hover:w-full md:opacity-0 md:group-hover:opacity-100 `}
+                ></span>
+              </NavLink>
+            </li>
+            <li
+              className="transform transition-all duration-300 hover:translate-x-1 md:hover:translate-x-0 pt-2"
+              onClick={closeMenu}
+            >
+              <NavLink
+                to={"/our-products/"}
+                className={({ isActive }) =>
+                  `block py-2 px-3 md:p-0 rounded relative transform transition-all duration-300 ease-in-out 
+                    group ${
+                      isActive
+                        ? "text-primary bg-green-50/60 md:bg-transparent md:text-primary"
+                        : "text-text/90 hover:bg-primary md:hover:bg-transparent md:hover:text-primary nav-content"
+                    }`
+                }
+              >
+                Resources & Blogs
+                <span
+                  className={`absolute bottom-0 left-0 w-0 h-0.5 bg-primary transition-all duration-300 ease-out 
+                                 group-hover:w-full md:opacity-0 md:group-hover:opacity-100 `}
+                ></span>
+              </NavLink>
+            </li>
+            <li
+              className="transform transition-all duration-300 hover:translate-x-1 md:hover:translate-x-0 pt-2"
+              onClick={closeMenu}
+            >
+              <NavLink
+                to={"/our-products/"}
+                className={({ isActive }) =>
+                  `block py-2 px-3 md:p-0 rounded relative transform transition-all duration-300 ease-in-out 
+                    group ${
+                      isActive
+                        ? "text-primary bg-green-50/60 md:bg-transparent md:text-primary"
+                        : "text-text/90 hover:bg-primary md:hover:bg-transparent md:hover:text-primary nav-content"
+                    }`
+                }
+              >
+                Industrial Connections
+                <span
+                  className={`absolute bottom-0 left-0 w-0 h-0.5 bg-primary transition-all duration-300 ease-out 
+                                 group-hover:w-full md:opacity-0 md:group-hover:opacity-100 `}
+                ></span>
               </NavLink>
             </li>
           </ul>
         </div>
+        <button className="bg-primary/10 text-primary text-sm font-semibold font-raleway cursor-pointer py-2 px-5 w-auto rounded-full duration-300 z-10 flex flex-row group ease-in">
+          Go to Dashboard
+        </button>
       </div>
     </nav>
   );
